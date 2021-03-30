192aa6bb91bfa82adfc0216e014ef2f57d86db03
Jirakit <chobthumkit_j@silpakorn.edu>
2021-03-30 14:59:45 +0700

    Commit Complete

---
+++ app/OrderDetail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    //
+    public function item() {
+        return $this->belongsTo(Item::class);
+    }
+}
